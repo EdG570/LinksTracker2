c0f8e083a0d4fdcc4038774b89b5c1ae9c3bd64e
EdG570 <cole570@hotmail.com>
2019-03-09 15:21:01 -0500

    Created Stats class

---
+++ LinksTracker2/Models/Stats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinksTracker2.Models
+{
+    public class Stats
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool FIR { get; set; }
+        public bool GIR { get; set; }
+        public bool UpAndDown { get; set; }
+        public int Putts { get; set; }
+        public int Penalties { get; set; }
+        public int Score { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int HoleId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("HoleId")]
+        public virtual Hole Hole { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
