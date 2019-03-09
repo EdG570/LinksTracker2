46759c0cdeef871176f3eb4e38b4549ef5a84136
EdG570 <cole570@hotmail.com>
2019-03-09 12:42:46 -0500

    Add project files.

---
+++ LinksTracker2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinksTracker2.Startup))]
+namespace LinksTracker2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
