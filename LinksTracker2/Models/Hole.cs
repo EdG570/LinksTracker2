5f534e8e4108c289245b1749255cd4a727dcd626
EdG570 <cole570@hotmail.com>
2019-03-09 15:17:57 -0500

    Created Hole class

--- LinksTracker2/Models/Hole.cs
+++ LinksTracker2/Models/Hole.cs
@@ -1,6 +1,17 @@
-﻿namespace LinksTracker2.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LinksTracker2.Models
 {
     public class Hole
     {
+        [Key]
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int Yardage { get; set; }
+        public int Par { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int CourseId { get; set; }
     }
 }
