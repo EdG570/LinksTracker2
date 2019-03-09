c8d668ade1e951e1d6c65db0f3c5061c23eec8a2
EdG570 <cole570@hotmail.com>
2019-03-09 17:39:00 -0500

    Created IRepository interface

--- LinksTracker2/Controllers/PlayController.cs
+++ LinksTracker2/Controllers/PlayController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace LinksTracker2.Controllers
 {
     public class PlayController : Controller
     {
         // GET: Play
-        public ActionResult Index()
+        public ActionResult SelectCourse()
         {
             return View();
         }
---
+++ LinksTracker2/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace LinksTracker2
+{
+    public interface IRepository<TEnt, in TPk> where TEnt : class
+    {
+        TEnt FindOne(TPk id);
+        IEnumerable<TEnt> GetAll();
+        void Update(TEnt record);
+        void Delete(TEnt record);
+        TEnt Create(TEnt record);
+        void Dispose();
+        void Save();
+    }
+}
